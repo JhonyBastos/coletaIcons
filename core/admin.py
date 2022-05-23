25d8892103a694b95b3839232568a9dd49c9a229
JhonyBastos <jb_moraes@outlook.com>
2022-05-23 17:18:35 -0300

    Implementação do banco de dados e da tabela de ícons

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Icons
 
 # Programa que efetua a coleta de ícones web em Python utilizando a biblioteca BeautifulSoup.
+
+class IconsAdmin(admin.ModelAdmin):
+    list_display = ('icons')
+
+admin.site.register(Icons)
--- core/models.py
+++ core/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Icons(models.Model):
+    icons = models.CharField(max_length=100)
 
+    class Meta:
+        db_table = 'icons'
+
+    def __str__(self):
+        return self.icons      #obriga o admin do django a apresentar os icons pelo nome
